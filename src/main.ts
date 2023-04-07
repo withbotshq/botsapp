65d58fffdb7516655c09fd463d1996b8822b2649
Jonathan Clem <j@jclem.me>
2023-04-07 19:12:17 -0400

    No devTools by default

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ const createWindow = () => {
   }
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
