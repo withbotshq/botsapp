b2bda4ce9f0813ff8d411e67879c91488f23a43f
Max Schoening <max@max.wtf>
2023-04-12 08:00:19 -0700

    Add config for notarizing

--- forge.config.js
+++ forge.config.js
@@ -1,7 +1,14 @@
 /* eslint-env node */
 module.exports = {
   packagerConfig: {
-    icon: 'icons/chat.icns'
+    icon: 'icons/chat.icns',
+    osxSign: {},
+    osxNotarize: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID
+    }
   },
   rebuildConfig: {},
   makers: [
