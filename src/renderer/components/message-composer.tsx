41d7bc8655aaf9c10e2577c95ea97852ef6e31e9
Jonathan Clem <j@jclem.me>
2023-04-13 18:37:46 -0400

    Reset input height after submit

--- src/renderer/components/message-composer.tsx
+++ src/renderer/components/message-composer.tsx
@@ -34,13 +34,14 @@ export const MessageComposer: FC<{onSubmit: (content: string) => void}> = ({
           onKeyDown={e => {
             if (e.key === 'Enter' && !e.shiftKey) {
               e.preventDefault()
+              assert(e.currentTarget.parentElement).dataset.replicatedValue = ''
               submitForm()
             }
           }}
           onChange={e => {
             setNewMessageContent(e.target.value)
             assert(e.currentTarget.parentElement).dataset.replicatedValue =
-              e.target.value
+              e.currentTarget.value
           }}
           placeholder="Your message..."
           value={newMessageContent}
