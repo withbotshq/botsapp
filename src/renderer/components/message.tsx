de473aa901d0c258036b09c305df8ddb85ab0464
Max Schoening <max@max.wtf>
2023-04-13 18:47:38 -0700

    Preserve whitespace in messages

--- src/renderer/components/message.tsx
+++ src/renderer/components/message.tsx
@@ -12,7 +12,7 @@ export const MessageRenderer: FC<{message: Message}> = ({message}) => {
 
   return (
     <ReactMarkdown
-      className="markdown"
+      className="markdown whitespace-pre-wrap"
       components={{
         code({inline, className, children, ...props}) {
           const match = /language-(\w+)/.exec(className || '')
