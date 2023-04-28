42b1d9e22652234b67ef3991306c3d13877f4bd2
Jonathan Clem <j@jclem.me>
2023-04-28 16:56:48 -0400

    Add user button

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import {ClerkProvider} from '@clerk/nextjs/app-beta'
+import {ClerkProvider, UserButton} from '@clerk/nextjs/app-beta'
 import {FC, PropsWithChildren} from 'react'
 import '../styles/globals.css'
 
@@ -7,7 +7,15 @@ const RootLayout: FC<PropsWithChildren> = ({children}) => {
     <html lang="en">
       <head></head>
       <ClerkProvider>
-        <body>{children}</body>
+        <body>
+          <div className="flex flex-col gap-2">
+            <header className="flex h-12 flex-row items-center justify-end px-2 py-2">
+              <UserButton />
+            </header>
+
+            <main>{children}</main>
+          </div>
+        </body>
       </ClerkProvider>
     </html>
   )
--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,7 +1,7 @@
 import {FC} from 'react'
 
 const IndexPage: FC = () => {
-  return <h1 className="text-xl">Hello, world.</h1>
+  return <h1 className="mt-16 text-center text-7xl font-black">Bots</h1>
 }
 
 export default IndexPage
