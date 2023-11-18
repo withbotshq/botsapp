d458d84af0fc8abb0dd39f9e7f659130f33b7a56
Jonathan Clem <j@jclem.me>
2023-11-18 14:13:34 -0500

    Remove www app

--- apps/www/src/app/login/page.tsx
+++
@@ -1,8 +0,0 @@
-import {SignIn} from '@clerk/nextjs/app-beta'
-import {FC} from 'react'
-
-const LoginPage: FC = () => {
-  return <SignIn signUpUrl="/signup" />
-}
-
-export default LoginPage
--- apps/www/src/app/signup/page.tsx
+++
@@ -1,8 +0,0 @@
-import {SignUp} from '@clerk/nextjs/app-beta'
-import {FC} from 'react'
-
-const SignupPage: FC = () => {
-  return <SignUp signInUrl="/login" />
-}
-
-export default SignupPage
